5831a1eb3bf128d7574436392019df27cfce4b85
Hennemuth <wrh@bu.edu>
2023-11-21 00:39:55 -0500

    Adding baseline.sh

---
+++ baseline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Place all content from the specified branch on top of the corresponding
+# locations in /etc/httpd, overwriting existing files, else creating new.
+# EXAMPLE: sh baseline.sh prod
+refresh() {
+  local branch="$1"
+  [ -z "$branch" ] && echo "Branch not specified!" && return;
+
+  for file in $(git ls-tree -r --name-only $branch) ; do
+    echo $file; 
+    git show ${branch}:${file} > /etc/httpd/$file
+    break;
+  done
+}
+
+refresh $@
